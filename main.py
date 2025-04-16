73a4f0e88b6040fc2f17e89f83c4c200eb133263
Shariar Imam Shafin <imanshariar@gmail.com>
2025-04-17 02:52:06 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
+import ota_updater
+
+if ota_updater.check_for_update("https://raw.githubusercontent.com/yourusername/yourrepo/main"):
+    print("Updated code is now running.")
+
 print ("1234567890")
