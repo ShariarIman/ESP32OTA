49f2cfbe732a848a0b680aa4dcad861f17f4abe2
Shariar Imam Shafin <imanshariar@gmail.com>
2025-05-13 12:23:28 +0600

    Update main.py

--- main.py
+++ main.py
@@ -11,9 +11,9 @@ import time
 from machine import Pin
 
 # Define LED pins
-led2 = Pin(2, Pin.OUT)
-led4 = Pin(4, Pin.OUT)
-led17 = Pin(17, Pin.OUT)
+led1 = Pin(17, Pin.OUT)
+led2 = Pin(4, Pin.OUT)
+led3 = Pin(2, Pin.OUT)
 
 # LED pattern sequence (each tuple is a step: (GPIO2, GPIO4, GPIO17))
 pattern = [
@@ -25,7 +25,7 @@ pattern = [
 
 while True:
     for state in pattern:
-        led2.value(state[0])
-        led4.value(state[1])
-        led17.value(state[2])
+        led1.value(state[0])
+        led2.value(state[1])
+        led3.value(state[2])
         time.sleep(0.5)
