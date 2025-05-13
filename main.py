8c14bb2b794b4f9743d500452865be776f10f1a3
Shariar Imam Shafin <imanshariar@gmail.com>
2025-05-13 11:59:25 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 import ota_updater
 
 if ota_updater.check_for_update("https://raw.githubusercontent.com/ShariarIman/ESP32OTA/main", verbose=True):
-    print("Hello OTA 1.3.1")
     print("[OTA] Running updated code.")
 
 print("Running main program.")
+import time
+from machine import Pin
+
+led = Pin(2, Pin.OUT)
+
+while True:
+    led.on()
+    time.sleep(0.5)
+    led.off()
+    time.sleep(0.5)
