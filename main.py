62ede43887ad806ebb0063537a0be02d13a7d441
Shariar Imam Shafin <imanshariar@gmail.com>
2025-05-13 11:39:41 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import ota_updater
 
 if ota_updater.check_for_update("https://raw.githubusercontent.com/ShariarIman/ESP32OTA/main", verbose=True):
+    print("Hello OTA 1.3.1")
     print("[OTA] Running updated code.")
 
 print("Running main program.")
